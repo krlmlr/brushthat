e210b48d735b37df79f527fd25b7de9c2ae44d43
Kirill Müller <krlmlr+r@mailbox.org>
2017-05-10 21:10:30 +0200

    simplify UI

--- R/shine.R
+++ R/shine.R
@@ -1,13 +1,11 @@
 # We'll wrap our Shiny Gadget in an addin.
 # Let's call it 'clockAddin()'.
-shine <- function() {
+shine <- function(pkg = ".") {
   # Our ui will be a simple gadget page, which
   # simply displays the time in a 'UI' output.
   ui <- miniPage(
-    gadgetTitleBar("brushthat", left = NULL),
+    gadgetTitleBar("brushthat", left = miniTitleBarButton("run", "Run test", primary = TRUE)),
     miniContentPanel(
-      textInput("path", "Path", getwd()),
-      actionButton("run", "Run test"),
       radioButtons("failures", "Failing tests", choices = "No test failures found"),
       textOutput("status", inline = TRUE)
     )
