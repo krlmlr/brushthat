e25b9a995ef7a44f8d9de7694bc2c41beb5f85b7
Kirill Müller <krlmlr+r@mailbox.org>
2017-05-11 23:22:54 +0200

    basic call stack navigation

--- R/shine.R
+++ R/shine.R
@@ -6,7 +6,10 @@ ui <- miniPage(
     left = miniTitleBarButton("run", "Run test", primary = TRUE)
   ),
   miniContentPanel(
-    radioButtons("failures", "Failing tests", choices = "No test failures found"),
+    splitLayout(
+      radioButtons("results", "Tests", choices = "No tests found"),
+      radioButtons("call_stack", "Call stack", choices = "No test selected")
+    ),
     textOutput("status", inline = TRUE)
   )
 )
@@ -14,9 +17,12 @@ ui <- miniPage(
 # We'll wrap our Shiny Gadget in an addin.
 # Let's call it 'clockAddin()'.
 shine <- function(pkg = ".") {
+  pkg <- normalizePath(pkg, winslash = "/")
+
   server <- function(input, output, session) {
 
     results <- NULL
+    call_stack_df <- NULL
 
     output$status <- renderText({
       reporter <- BrushReporter$new()
@@ -31,10 +37,14 @@ shine <- function(pkg = ".") {
       )
       choices <- set_names(seq_along(results), test_names)
 
-      updateRadioButtons(session, "failures", choices = choices)
+      updateRadioButtons(session, "results", choices = choices)
 
       paste0("Runs: ", input$run)
     })
+
+    observe(call_stack_df <<- fill_call_stack(session, results[[as.integer(input$results)]], pkg))
+
+    observe(navigate_call_stack_entry(call_stack_df, as.numeric(input$call_stack)))
 
     # Listen for 'done' events. When we're finished, we'll
     # insert the current time, and then stop the gadget.
@@ -53,4 +63,35 @@ shine <- function(pkg = ".") {
 
 result_type <- function(result) {
   toupper(substr(gsub("^expectation_", "", class(result)[[1]]), 1, 1))
+}
+
+fill_call_stack <- function(session, result, pkg) {
+  if (is.null(result)) return()
+
+  srcrefs <- lapply(result$call, attr, "srcref")
+  has_ref <- map_lgl(srcrefs, inherits, "srcref")
+  valid_srcrefs <- srcrefs[has_ref]
+  files <- map_chr(valid_srcrefs, function(x) attr(x, "srcfile")$filename)
+  files <- gsub(paste0(pkg, "/"), "", files)
+  lines <- map_int(valid_srcrefs, function(x) as.vector(x)[1])
+  columns <- map_int(valid_srcrefs, function(x) as.vector(x)[2])
+
+  addr <- paste0(files, ":", lines, ":", columns)
+  choices <- set_names(seq_along(addr), addr)
+
+  updateRadioButtons(session, "call_stack", choices = choices, selected = 1L)
+
+  call_stack_df <- data.frame(
+    file = files, line = lines, column = columns,
+    stringsAsFactors = FALSE
+  )
+  navigate_call_stack_entry(call_stack_df, 1L)
+  call_stack_df
+}
+
+navigate_call_stack_entry <- function(call_stack_df, call_stack_pos) {
+  if (is.na(call_stack_pos)) return()
+  file_pos <- call_stack_df[call_stack_pos, , drop = FALSE]
+  print(file_pos)
+  rstudioapi::navigateToFile(file_pos$file, file_pos$line, file_pos$column)
 }
